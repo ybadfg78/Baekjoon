d94147bf909791c083565f228a371c23cc338f11
ybadfg78 <ybadfg78@gmail.com>
2024-05-21 00:53:17 +0900

    백준 문제풀이

---
+++ Baekjoon/1008.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Baekjoon1008
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+            double p = double.Parse(ss[0]);
+            double q = double.Parse(ss[1]);
+            Console.WriteLine(p/q);
+
+        }
+    }
+}
---
+++ Baekjoon/10869.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Baekjoon10869
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            string[] ss = s.Split();
+            int p = int.Parse(ss[0]);
+            int q = int.Parse(ss[1]);
+            Console.WriteLine(p+q);
+            Console.WriteLine(p-q);
+            Console.WriteLine(p*q);
+            Console.WriteLine(p/q);
+            Console.WriteLine(p%q);
+
+        }
+    }
+}
