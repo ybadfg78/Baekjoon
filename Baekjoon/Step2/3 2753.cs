26b110b4d7b8751e9e39b39bc4944b19fe8a5bdc
ybadfg78 <ybadfg78@gmail.com>
2024-05-24 23:38:10 +0900

    24.05.24

---
+++ Baekjoon/Step2/3 2753.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Baekjoon2753
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            if (a % 4 == 0)
+            {
+                if (a % 100 == 0)
+                {
+                    if (a % 400 == 0)
+                    {
+                        Console.WriteLine("1");
+                    }
+                    else
+                    {
+                        Console.WriteLine("0");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("1");
+                }
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
