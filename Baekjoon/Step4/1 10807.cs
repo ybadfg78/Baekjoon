91087105fe546b7efb4d99e13415d8100d4fdffb
ybadfg78 <ybadfg78@gmail.com>
2024-05-26 19:57:54 +0900

    24.05.26

---
+++ Baekjoon/Step4/1 10807.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Baekjoon10807
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int t = int.Parse(Console.ReadLine());
+            string[] ss = Console.ReadLine().Split();
+            int k = int.Parse(Console.ReadLine());
+            int result = 0;
+            for (int i = 0; i < t; i++)
+            {
+                int s = int.Parse(ss[i]);
+                if (s == k)
+                {
+                    result++;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
